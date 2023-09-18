e739a742fd2b535df841bf74828aeb3eab8f0cc9
RDKS14 <rob.smith7@hotmail.co.uk>
2023-09-18 10:56:31 +0100

    Spring1

---
+++ src/main/java/com/Sky/Vehiclesdemo/VehiclesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.Sky.Vehiclesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclesDemoApplication.class, args);
+	}
+
+}
